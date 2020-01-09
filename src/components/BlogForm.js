8be57eda231956573454ff8dff086a7a87d74400
Enrique <enriquecardenas96@gmail.com>
2020-01-09 15:02:44 -0800

    add prop types to blogform and togglable

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const BlogForm = ({ 
   onSubmit,
@@ -44,6 +45,16 @@ const BlogForm = ({
     </form>
   </div>
   )
+}
+
+BlogForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  handleTitleChange: PropTypes.func.isRequired,
+  handleAuthorChange: PropTypes.func.isRequired,
+  handleUrlChange: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 }
 
 export default BlogForm
